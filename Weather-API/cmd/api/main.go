9b5f813bc38b5ac8dd52e57edad79556b5c11b7a
bianavic <bianavic@gmail.com>
2025-04-12 13:28:51 -0300

    feat: setup a webserver

--- Weather-API/cmd/api/main.go
+++ Weather-API/cmd/api/main.go
@@ -1,1 +1,35 @@
-package api
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	log.Print("starting server...")
+	http.HandleFunc("/weather", handler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
+	}
+
+	server := &http.Server{
+		Addr:         ":8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	fmt.Println("Server running on port 8080")
+	if err := server.ListenAndServe(); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World! What will the weather be like today?"))
+}
